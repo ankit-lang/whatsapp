5fe57bc574ea3db03b028b3ec6e6b611f5e37d04
Ankit kumar <itzankitrajput@users.noreply.huggingface.co>
2026-06-01 10:53:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 let currentSessionState = {
     pairingCode: null,
     isReady: false,
-    initializationTarget: '31630645930' // 👈 Set Lavina's absolute destination number (with country code, no symbols) here
+    initializationTarget: '918178573528' // 👈 Set Lavina's absolute destination number (with country code, no symbols) here
 };
 
 // 2. Enable Cross-Origin Resource Sharing (CORS)
